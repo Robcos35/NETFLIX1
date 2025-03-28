8d5e51205b29fe475e8cca8dcf613f5f33d4863d
Robcos35 <rcosentino04@gmail.com>
2025-03-28 11:55:24 +0100

    Aggiungere i file di progetto.

---
+++ App_Code/CATEGORIE.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+public class CATEGORIE
+{
+	public int K_Categoria;
+	public string Categoria;
+
+	public CATEGORIE()
+	{
+	}
+	//SelectAll
+	public DataTable SelectAll()
+	{
+		DB db = new DB();
+		db.query = "Categorie_SelectAll";
+		return db.SQLselect();
+	}
+	//Verifica per inserimento
+	public DataTable Verifica()
+	{
+		DB db = new DB();
+		db.query = "Categorie_Verifica";
+		db.cmd.Parameters.AddWithValue("@cat", Categoria);
+		return db.SQLselect();
+	}
+	//Inserimento
+	public void Insert()
+	{
+		DB db = new DB();
+		db.query = "Categorie_Insert";
+		db.cmd.Parameters.AddWithValue("@cat", Categoria);
+		db.Sqlcommand();
+	}
+	//Select per chiave
+	public DataTable SelectChiave()
+	{
+		DB db = new DB();
+		db.query = "Categorie_SelectChiave";
+		db.cmd.Parameters.AddWithValue("@chiave", K_Categoria);
+		return db.SQLselect();
+	}
+    //Verifica per update
+    public DataTable VerificaUpdate()
+	{
+		DB db = new DB();
+		db.query = "Categorie_VerificaUpdate";
+        db.cmd.Parameters.AddWithValue("@chiave", K_Categoria);
+        db.cmd.Parameters.AddWithValue("@cat", Categoria);
+		return db.SQLselect();
+    }
+	//Update
+	public void Update()
+	{
+		DB db = new DB();
+		db.query = "Categorie_Update";
+        db.cmd.Parameters.AddWithValue("@chiave", K_Categoria);
+        db.cmd.Parameters.AddWithValue("@cat", Categoria);
+		db.Sqlcommand();
+    }
+}
---
+++ App_Code/DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+
+public class DB
+{
+    SqlConnection conn = new SqlConnection();
+    public SqlCommand cmd = new SqlCommand();
+    SqlDataAdapter DA = new SqlDataAdapter();
+    public string query;
+
+    public DB() //COSTRUTTORE
+    {
+        conn.ConnectionString = "Data Source=LAPTOP-V3K3MMCB\\SQLEXPRESS;Initial Catalog=NETFLIX; Integrated Security=true";
+        cmd.Connection = conn;
+        cmd.CommandType = CommandType.StoredProcedure;
+    }
+
+    public DataTable SQLselect()
+    {
+        DataTable DT = new DataTable();
+        cmd.CommandText = query;
+        DA.SelectCommand = cmd;
+        DA.Fill(DT);
+        return DT;
+    }
+
+    public void Sqlcommand()
+    {
+        cmd.CommandText = query;
+        //parametri
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+}
